39878689759847e87be76563f13801ee721637ff
Derek Frye <derek.frye@gmail.com>
2025-01-10 23:47:09 -0600

    Nearly done. Doesn't quite line-up.
    - But going to move in new direction.

--- src/view/score.rs
+++ src/view/score.rs
@@ -7,7 +7,7 @@ pub fn render_scores_template(data: &ScoreData) -> Markup {
     html! {
         @if !data.score_struct.is_empty() {
             (render_scoreboard(data))
-            // (render_summary_scores(data))
+            (render_summary_scores(data))
             (render_stacked_bar_chart(data))
             (render_score_detail(data))
         }
@@ -79,41 +79,41 @@ fn render_scoreboard(data: &ScoreData) -> Markup {
     }
 }
 
-// fn render_summary_scores(data: &ScoreData) -> Markup {
-//     html! {
-//         @let summary_scores = group_by_bettor_name_and_round(&data.score_struct);
-//
-//         @if !summary_scores.summary_scores.is_empty() {
-//             h3 { "Summary" }
-//
-//             table class="summary" {
-//                 thead {
-//                     tr {
-//                         th { "Player" }
-//                         @let num_rounds = summary_scores.summary_scores[0].computed_rounds.len();
-//                         @for round in 0..num_rounds {
-//                             th { "R" (round + 1) }
-//                         }
-//                         th { "Tot" }
-//                     }
-//                 }
-//                 tbody {
-//                     @for summary_score in &summary_scores.summary_scores {
-//                         tr {
-//                             td { (summary_score.bettor_name) }
-//                             @for (idx, _round) in summary_score.computed_rounds.iter().enumerate() {
-//                                 @let score = summary_score.new_scores[idx];
-//                                 td { (score) }
-//                             }
-//                             @let total = summary_score.new_scores.iter().sum::<isize>();
-//                             td { (total) }
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
+fn render_summary_scores(data: &ScoreData) -> Markup {
+    html! {
+        @let summary_scores = group_by_bettor_name_and_round(&data.score_struct);
+
+        @if !summary_scores.summary_scores.is_empty() {
+            h3 { "Summary" }
+
+            table class="summary" {
+                thead {
+                    tr {
+                        th { "Player" }
+                        @let num_rounds = summary_scores.summary_scores[0].computed_rounds.len();
+                        @for round in 0..num_rounds {
+                            th { "R" (round + 1) }
+                        }
+                        th { "Tot" }
+                    }
+                }
+                tbody {
+                    @for summary_score in &summary_scores.summary_scores {
+                        tr {
+                            td { (summary_score.bettor_name) }
+                            @for (idx, _round) in summary_score.computed_rounds.iter().enumerate() {
+                                @let score = summary_score.new_scores[idx];
+                                td { (score) }
+                            }
+                            @let total = summary_score.new_scores.iter().sum::<isize>();
+                            td { (total) }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 fn render_thead(max_len_of_tee_times_in_rounds: usize, group: &usize) -> Markup {
     html! {
@@ -219,57 +219,78 @@ fn render_score_detail(data: &ScoreData) -> Markup {
 
 fn render_stacked_bar_chart(data: &ScoreData) -> Markup {
     html! {
-    @let summary_scores = group_by_bettor_name_and_round(&data.score_struct);
+        @let summary_scores = group_by_bettor_name_and_round(&data.score_struct);
 
-    @if !summary_scores.summary_scores.is_empty() {
-        h3 { "Summary" }
+        @if !summary_scores.summary_scores.is_empty() {
+            h3 { "Summary" }
 
-        div class="stacked-bar-chart" {
+            div class="stacked-bar-chart" {
 
-            @let min_score: isize = summary_scores.summary_scores.iter()
-                .map(|summary_score| summary_score.new_scores.iter().sum::<isize>())
-                .collect::<Vec<isize>>()
-                .into_iter().min().unwrap_or(0).abs();
+                @let min_score: isize = summary_scores.summary_scores.iter()
+                    .map(|summary_score| summary_score.new_scores.iter().sum::<isize>())
+                    .collect::<Vec<isize>>()
+                    .into_iter().min().unwrap_or(0).abs();
 
-            @let max_scorea: isize = summary_scores.summary_scores.iter()
-                .map(|summary_score| summary_score.new_scores.iter().sum::<isize>())
-                .collect::<Vec<isize>>()
-                .into_iter().max().unwrap_or(0).abs();
+                @let max_scorea: isize = summary_scores.summary_scores.iter()
+                    .map(|summary_score| summary_score.new_scores.iter().sum::<isize>())
+                    .collect::<Vec<isize>>()
+                    .into_iter().max().unwrap_or(0).abs();
 
-            @let max_score = ((max_scorea.max(min_score) as f64) * 0.3 * 10.0).round() / 10.0;
+                @let max_ticks = (max_scorea.max(min_score) + 19) / 20 * 20; // Round up to the nearest 20
+                @let vert_axis_height = (max_ticks as f64) / 10.0;
+                @let max_ticks_scaled = ((max_ticks as f64) * 0.3 * 10.0).round() / 10.0;
 
-            @for summary_score in &summary_scores.summary_scores {
-                div class="bar-group" {
-                    @let total_score: isize = summary_score.new_scores.iter().sum();
+                div class="vertical-axis" style=(format!("position: absolute; width: 100%; height: {}rem; display: flex; flex-direction: column-reverse; gap: 0; z-index: 1;"
+                    , max_ticks_scaled )) {
 
-                    div class="total-label" { (total_score) }
+                    // need this here to make bottom of first tick line up with bottom of first bar
+                    div class="bar-group-label" {  }
 
-                    div class="bars" style=(format!("height: {}em; display: flex; align-items: flex-end;", max_score)) {
-                        @for (idx, _round) in summary_score.computed_rounds.iter().enumerate() {
-                            @let score = summary_score.new_scores[idx];
-                            div class="bar" style=(format!(
-                                "height: {}em; background-color: {};",
-                                    ((score.abs() as f64) * 0.3 * 10.0).round() / 10.0
-                                ,
-                                match idx {
-                                    0 => "#007BFF",
-                                    1 => "#FF5733",
-                                    2 => "#33FF57",
-                                    3 => "#FFC300",
-                                    4 => "#C70039",
-                                    5 => "#900C3F",
-                                    6 => "#581845",
-                                    _ => "#DAF7A6",
-                                }
-                            )) {
+                    @for tick in (0..=max_ticks).step_by(10) {
+                        div class="axis-line" style=(format!("position: relative; height: {}rem; display: flex; align-items: center;", vert_axis_height)) {
+                            span class="tick-label" style=("position: absolute; left: -0.05rem; font-size: 0.8rem;") {
+                                (tick)
                             }
+                            div class="tick-line" style=("width: 100%; height: 0.05rem; background-color: #ddd; ") {}
                         }
                     }
 
-                    div class="bar-group-label" { (summary_score.bettor_name) }
+                }
+
+                div class="chart-body" style=(format!("position: relative; width: 100%; height: {}rem; display: flex; align-items: flex-end; z-index: 2;", max_ticks_scaled)) {
+                    @for summary_score in &summary_scores.summary_scores {
+                        div class="bar-group" {
+                            @let total_score: isize = summary_score.new_scores.iter().sum();
+
+                            div class="total-label" { (total_score) }
+
+                            div class="bars" style=(format!("display: flex; flex-direction: column-reverse; gap: 0.06rem;")) {
+                                @for (idx, _round) in summary_score.computed_rounds.iter().enumerate() {
+                                    @let score = summary_score.new_scores[idx];
+                                    @let score_scaled = ((score.abs() as f64) / (max_ticks as f64) * max_ticks_scaled).round();
+                                    div class="bar" style=(format!(
+                                        "height: {}rem; background-color: {};",
+                                            score_scaled
+                                        ,
+                                        match idx {
+                                            0 => "#007BFF",
+                                            1 => "#FF5733",
+                                            2 => "#33FF57",
+                                            3 => "#FFC300",
+                                            4 => "#C70039",
+                                            5 => "#900C3F",
+                                            6 => "#581845",
+                                            _ => "#DAF7A6",
+                                        }
+                                    )) { }
+                                }
+                            }
+
+                            div class="bar-group-label" { (summary_score.bettor_name) }
+                        }
+                    }
                 }
             }
-        }
         }
     }
 }
