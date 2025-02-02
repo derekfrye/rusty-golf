ad4e1430517a910188cf134d44f3fa07a52a0936
Derek Frye <derek.frye@gmail.com>
2025-02-01 19:47:26 -0600

    Ready to test.

--- tests/test4.rs
+++ tests/test4.rs
@@ -1,13 +1,8 @@
-use serde_json::Value;
-use std::fs;
-use std::path::PathBuf;
-// use sqlx::sqlite::SqlitePoolOptions;
-use std::sync::Arc;
-use std::{ collections::HashMap, vec };
-use tokio::sync::RwLock;
+use rusty_golf::controller::db_prefill;
+use crate::db_prefill::db_prefill;
+// `, `use rusty_golf::controller::db_prefill::db_prefill;
 
 // use rusty_golf::controller::score;
-use rusty_golf::{ controller::score::get_data_for_scores_page, model::CacheMap };
 
 use sql_middleware::middleware::{
     AsyncDatabaseExecutor,
@@ -17,20 +12,12 @@ use sql_middleware::middleware::{
 };
 
 #[tokio::test]
-async fn get_scores_from_cache() -> Result<(), Box<dyn std::error::Error>> {
+async fn test_dbprefill() -> Result<(), Box<dyn std::error::Error>> {
     // Initialize logging (optional, but useful for debugging)
     // let _ = env_logger::builder().is_test(true).try_init();
 
-    let x = "xxx".to_string();
-    let database_exists = {
-        let path = PathBuf::from(&x);
-        if !path.is_file() || !fs::metadata(&path).is_ok() {
-            false
-        } else {
-            true
-        }
-    };
-    let config_and_pool = ConfigAndPool2::new_sqlite(x.clone()).await.unwrap();
+    let x = "file::memory:?cache=shared".to_string();
+    let config_and_pool = ConfigAndPool2::new_sqlite(x).await.unwrap();
 
     let ddl = vec![
         include_str!("../src/admin/model/sql/schema/sqlite/00_event.sql"),
@@ -48,99 +35,48 @@ async fn get_scores_from_cache() -> Result<(), Box<dyn std::error::Error>> {
 
     let pool = config_and_pool.pool.get().await?;
     let mut conn = MiddlewarePool::get_connection(pool).await?;
-
     conn.execute_batch(&query_and_params.query).await?;
 
-    if !database_exists {
-        let setup_queries = include_str!("test1.sql");
-        let query_and_params = QueryAndParams {
-            query: setup_queries.to_string(),
-            params: vec![],
-        };
+    // first verify that nothing is in these tables
+    let query = "select * from event;";
+    let res = conn.execute_select(&query, &vec![]).await?;
+    assert_eq!(res.results. len(), 0);
+    let query = "select * from golfer;";
+    let res = conn.execute_select(&query, &vec![]).await?;
+    assert_eq!(res.results. len(), 0);
+    let query = "select * from bettor;";
+    let res = conn.execute_select(&query, &vec![]).await?;
+    assert_eq!(res.results. len(), 0);
+    let query = "select * from event_user_player;";
+    let res = conn.execute_select(&query, &vec![]).await?;
+    assert_eq!(res.results. len(), 0);
 
-        conn.execute_batch(&query_and_params.query).await?;
-    }
+    let json = serde_json::from_str(include_str!("test5_dbprefill.json"))?;
+    db_prefill(&json, &config_and_pool).await?;
+    
+    // now verify that the tables have been populated
+    let query = "select * from event;";
+    let res = conn.execute_select(&query, &vec![]).await?;
+    assert_eq!(res.results. len(), 1);
 
-    let cache_map: CacheMap = Arc::new(RwLock::new(HashMap::new()));
-    let score_data = (match database_exists {
-        true =>
-            // cache max age of 0 means always use db, since model.event_and_scores_already_in_db does this:
-            // let now = chrono::Utc::now().naive_utc();
-            // let diff = now - z?;
-            // Ok(diff.num_days() > cache_max_age)
-            get_data_for_scores_page(401580351, 2024, &cache_map, true, &config_and_pool, 0).await,
-        false =>
-            get_data_for_scores_page(
-                401580351,
-                2024,
-                &cache_map,
-                false,
-                &config_and_pool,
-                99
-            ).await,
-    })?;
+    let query = "select * from golfer;";
+    let res = conn.execute_select(&query, &vec![]).await?;
+    assert_eq!(res.results. len(), 22);
+    let x = res.results.iter().find(|z| *z.get("espn_id").unwrap().as_int().unwrap() == 4375972);
+    assert_eq!(x.is_some(), true);
+    assert_eq!(x.unwrap().get("name").unwrap().as_text().unwrap(), "Ludvig Åberg");
+    
+    let query = "select * from bettor;";
+    let res = conn.execute_select(&query, &vec![]).await?;
+    assert_eq!(res.results. len(), 5);
+    let x = res.results.iter().find(|z| z.get("name").unwrap().as_text().unwrap() == "Player5");
+    assert_eq!(x.is_some(), true);
 
-    // load reference json
-    let reference_result_str = include_str!("test3_espn_json_responses.json");
-    let reference_result: Value = serde_json::from_str(reference_result_str).unwrap();
-
-    let bryson_espn_entry = score_data.score_struct
-        .iter()
-        .find(|s| s.golfer_name == "Bryson DeChambeau")
-        .unwrap();
-    let bryson_reference_entry = reference_result
-        .get("score_struct")
-        .unwrap()
-        .as_array()
-        .unwrap()
-        .iter()
-        .find(|s| s.get("golfer_name").unwrap() == "Bryson DeChambeau")
-        .unwrap();
-    assert_eq!(
-        bryson_espn_entry.detailed_statistics.total_score as i64,
-        bryson_reference_entry
-            .get("detailed_statistics")
-            .unwrap()
-            .get("total_score")
-            .unwrap()
-            .as_i64()
-            .unwrap()
-    );
-    assert_eq!(
-        bryson_espn_entry.eup_id,
-        bryson_reference_entry.get("eup_id").unwrap().as_i64().unwrap()
-    );
-
-    // let xx = serde_json::to_string_pretty(&x).unwrap();
-    // println!("{}", xx); // test3_scoredata.json
-    // let a = 1 + 1;
-
-    let left = bryson_reference_entry
-        .get("detailed_statistics")
-        .unwrap()
-        .get("line_scores")
-        .unwrap()
-        .as_array()
-        .unwrap()
-        .iter()
-        .find(|s| {
-            s.get("hole").unwrap().as_i64().unwrap() == 13 &&
-                s.get("round").unwrap().as_i64().unwrap() == 2
-        })
-        .unwrap()
-        .get("score")
-        .unwrap()
-        .as_i64()
-        .unwrap() as i32;
-    println!("{}", left);
-    let right = bryson_espn_entry.detailed_statistics.line_scores
-        .iter()
-        .find(|s| s.hole == 13 && s.round == 2)
-        .unwrap().score;
-    println!("{}", right);
-
-    assert_eq!(left, right);
-    assert_eq!(left, 3); // line 6824 in test3_espn_json_responses.json
+    let query = "select * from event_user_player;";
+    let res = conn.execute_select(&query, &vec![]).await?;
+    assert_eq!(res.results. len(), 15);
+    let x = res.results.iter().find(|z| z.get("bettor").unwrap().as_text().unwrap() == "Player3" && *z.get("golfer_espn_id").unwrap().as_int().unwrap() == 9780);
+    assert_eq!(x.is_some(), true);
 
     Ok(())
 }
