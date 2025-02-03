ac6b5a43b2457dfc285f69bffe125b31ee206dc5
Derek Frye <derek.frye@gmail.com>
2025-02-02 21:09:50 -0600

    rip out old code

--- src/view/score.rs
+++ src/view/score.rs
@@ -1,6 +1,5 @@
 use std::collections::BTreeMap;
 
-use crate::controller::score::group_by_scores;
 use crate::get_title_and_score_view_conf_from_db;
 use crate::model::{
     get_scores_from_db, AllBettorScoresByRound, DetailedScore, LineScore, RefreshSource, ScoreData, ScoreDisplay, ScoresAndLastRefresh, StringStat, SummaryDetailedScores
@@ -148,120 +147,7 @@ fn render_summary_scores(grouped_data: &AllBettorScoresByRound) -> Markup {
     }
 }
 
-fn render_thead(max_len_of_tee_times_in_rounds: usize, group: &usize) -> Markup {
-    html! {
-        thead {
-            tr {
-                th class="topheader" rowspan="2" {
-                    "Player"
-                }
-                th class="topheader" rowspan="2" {
-                    "Pick"
-                }
 
-                @for round in 0..max_len_of_tee_times_in_rounds {
-                    th class="topheader shrinkable" colspan="3" data-round=({ round + 1 }) {
-                        span class="toggle" data-round=({ round + 1 }) onclick=(format!("toggleRound({})", round + 1)) {
-                            "Round " (round + 1)
-                        }
-                        br;
-                        span class="kindatiny" data-round=({ round + 1 }) onclick=(format!("toggleRound({})", round + 1)) {
-                            "Tap to shrink"
-                        }
-                    }
-                }
-                th class="topheader" {
-                    i {
-                        "Totals"
-                    }
-                }
-            }
-            tr {
-                @let z_vec = ["Tee Time (CT)", "Holes Compl.", "Score"];
-                @let z_len = z_vec.len();
-                @for round in 0..max_len_of_tee_times_in_rounds {
-                    // for each of the 4 columns, add tee time, holes completed, and score
-                    @for a in 0..z_len {
-                        @let c = round * z_vec.len() + a +1;
-                        th class="sortable hideable" data-round=({
-                            round + 1
-                        })
-                        onclick=(format!("sortTable('scores-table-{}', {})", group, c)) {
-                            (z_vec[a])
-                        }
-                    }
-                }
-                @let d = max_len_of_tee_times_in_rounds * z_vec.len() +1;
-                th class="sortable" onclick=(format!("sortTable('scores-table-{}', {})", group, d)) {
-                    "Total"
-                }
-            }
-        }
-    }
-}
-
-fn render_tee_time_detail(data: &ScoreData) -> Markup {
-    html! {
-        h4 class="playerdetails" { "Tee Time Details" }
-
-        div class="playerdetails" {
-            button class="playerdetailsbtn" onclick="toggleAllPlayersDetailDiv()" {
-                "Click to show/hide details"
-            }
-
-            div class="playerdetailsdiv" style="display: none;" {
-                p class="playerdetailsmsg" { "Showing details for all players. You can further filter by clicking links above." }
-
-                @let grouped_scores = group_by_scores(data.score_struct.clone());
-                @for (group, scores) in &grouped_scores {
-                    @let max_len_of_tee_times = scores.iter().map(|score| score.detailed_statistics.tee_times.len()).max().unwrap_or(0);
-                    table id=(format!("scores-table-{}", group)) {
-                        (render_thead(max_len_of_tee_times, group) )
-                        tbody {
-                            @for score in scores {
-                                tr class="playerrow" data-player=(score.bettor_name) {
-                                    td { (score.bettor_name) }
-                                    td { (score.golfer_name) }
-
-                                    @let stats = &score.detailed_statistics;
-                                    @for index in 0..max_len_of_tee_times {
-                                        @let tee_time_len = stats.tee_times.len();
-                                        @if index < tee_time_len {
-                                            td class="cells hideable teetime" data-round=({ index + 1 }) {
-                                                (stats.tee_times[index].val)
-                                            }
-                                            td class="cells hideable" data-round=({ index + 1 }) {
-                                                @if index < stats.holes_completed_by_round.len() {
-                                                    (stats.holes_completed_by_round[index].val)
-                                                } @else {
-                                                    "N/A"
-                                                }
-                                            }
-                                            td class="cells" data-round=({ index + 1 }) {
-                                                @if index < stats.round_scores.len() {
-                                                    (stats.round_scores[index].val)
-                                                } @else {
-                                                    "N/A"
-                                                }
-                                            }
-                                        } @else {
-                                            td class="cells hideable" data-round=({ index + 1 }) { "N/A" }
-                                            td class="cells hideable" data-round=({ index + 1 }) { "N/A" }
-                                            td class="cells" data-round=({ index + 1 }) { "N/A" }
-                                        }
-                                    }
-                                    td { (stats.total_score) }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                
-            }
-        }
-    }
-}
 
 // Structure to hold individual bar information
 struct Bar {
